17241fb508867a99b55aa1f23e5dedd7b6dfb2ce
Mert Cihan Bayır <mert.bayir@ogr.sakarya.edu.tr>
2026-05-01 13:48:51 +0300

    feat: otobüs koltuk seçimi, orta kapı, Google Sheets entegrasyonu ve iptal/değişim sistemi tamamlandı

---
+++ koltuk/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuZlZHf1tSuv3Lfv3K-CeObxdQZ932hkE",
+  authDomain: "belfu-9a7c4.firebaseapp.com",
+  projectId: "belfu-9a7c4",
+  storageBucket: "belfu-9a7c4.firebasestorage.app",
+  messagingSenderId: "87514453499",
+  appId: "1:87514453499:web:84240e535e0cc246e9e75e",
+  measurementId: "G-H8HVCHTN90"
+};
+
+window.firebaseConfig = firebaseConfig;
